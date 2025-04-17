c76bfa1f7c8ff748f7878a3eda947ca87b6e6314
Joseph Dadlez <jdadlez@ucsc.edu>
2025-04-16 21:02:56 -0700

    Still working on variables

--- rules.js
+++ rules.js
@@ -1,29 +1,24 @@
 class Start extends Scene {
     create() {
-        this.engine.setTitle(this.engine.storyData.Title); //DONE TODO: replace this text using this.engine.storyData to find the story title
+        this.engine.setTitle(this.engine.storyData.Title);
         this.engine.addChoice("Begin the story");
         console.log(this.engine.storyData);
     }
 
     handleChoice() {
-        this.engine.gotoScene(Location, this.engine.storyData.InitialLocation); // DONE TODO: replace this text by the initial location of the story
-        //console.log("#1",this.engine.storyData.InitialLocation);
+        this.engine.gotoScene(Location, this.engine.storyData.InitialLocation); 
     }
 }
 
 class Location extends Scene {
     create(key) {
-        let locationData = this.engine.storyData.Locations[key]; // DONE TODO: use `key` to get the data object for the current story location
+        let locationData = this.engine.storyData.Locations[key]; 
         
-        this.engine.show(locationData.Body); // DONE TODO: replace this text by the Body of the location data
-        // console.log("#2 Lets see:",this.engine.storyData.Locations[key]);
+        this.engine.show(locationData.Body); 
 
-        if(this.engine.storyData.Locations[key].Choices) { // Done TODO: check if the location has any Choices
-            //console.log("#3 check");
-            for(let choice of this.engine.storyData.Locations[key].Choices) { // Done TODO: loop over the location's Choices
-                //console.log("check #4");
-                this.engine.addChoice(choice.Text, choice); // Done TODO: use the Text of the choice
-                // Done TODO: add a useful second argument to addChoice so that the current code of handleChoice below works
+        if(this.engine.storyData.Locations[key].Choices) { 
+            for(let choice of this.engine.storyData.Locations[key].Choices) { 
+                this.engine.addChoice(choice.Text, choice); 
             }
         } else {
             this.engine.addChoice("The end.")
@@ -46,5 +41,7 @@ class End extends Scene {
         this.engine.show(this.engine.storyData.Credits);
     }
 }
+
+
 
 Engine.load(Start, 'myStory.json');
